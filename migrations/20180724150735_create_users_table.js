8889a2d09e2ff032ea9b8f2d10d6737a87806ca0
Unknown <thangytp@gmail.com>
2018-07-25 00:37:15 +0700

    update migrate

--- migrations/20180724150735_create_users_table.js
+++ migrations/20180724150735_create_users_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function (t) {
+  		t.increments('id').primary();
+        t.string('name').notNullable();
+        t.string('email', 100).unique();
+        t.string('password').notNullable();
+        t.timestamps();
+        t.timestamp('deleted_at').nullable();
+  	})
 };
 
 exports.down = function(knex, Promise) {
-  
+  	return knex.schema.dropTableIfExists('users')
 };
