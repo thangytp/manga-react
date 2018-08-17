2f8cfe2174ac73b1dc7aed96eab8a1cb6fc5837b
Unknown <thangytp@gmail.com>
2018-08-17 17:44:21 +0700

    update manga route

--- server/controller/v1/manga.js
+++ server/controller/v1/manga.js
@@ -1,9 +1,22 @@
 const { EXISTED_CODE, SUCCESS_CODE } = require('../../helper/ResponseCode');
 const ResponseTemplate = require('../../helper/ResponseTemplate');
+const Manga = require('../../models/Manga');
 
 class MangaController {
 	async list(req, res) {
+		try {
+			let page = req.page || 0;
+			let perpage = req.perpage || 50;
 
+			let listManga = await Manga.fetchPage({
+				pageSize: perpage,
+				page: page,
+			});
+			console.log(listManga);
+			return listManga;
+		} catch (e) {
+			console.log(e);
+		}
 	}
 
 	async create(req, res) {
--- server/routes/MangaRoute.js
+++ server/routes/MangaRoute.js
@@ -1,40 +1,15 @@
 const express = require('express');
 const app = express();
 const MangaRoute = express.Router();
-
-// Require Post model in our routes module
-let Manga = require('../models/Manga');
+const mangaController = require('../controller/v1/manga');
 
 // Defined store route
-MangaRoute.route('/add').post(function (req, res) {
-  let manga = new Manga(req.body);
-  manga.save()
-    .then(manga => {
-        res.status(200).json(manga);
-    })
-    .catch(err => {
-        res.status(400).send("unable to save to database");
-    });
-});
+MangaRoute.post('/create', mangaController.create);
 
 // Defined get data(index or listing) route
-MangaRoute.route('/').get(function (req, res) {
-    Manga.find(function (err, mangas){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.json(mangas);
-    }
-  });
-});
+MangaRoute.get('/', mangaController.list);
 
 // Defined delete | remove | destroy route
-MangaRoute.route('/delete/:id').get(function (req, res) {
-    Manga.findByIdAndRemove({_id: req.params.id}, function(err, manga){
-        if(err) res.json(err);
-        else res.json(req.params.id);
-    });
-});
+MangaRoute.delete('/delete/:id', mangaController.delete);
 
 module.exports = MangaRoute;
