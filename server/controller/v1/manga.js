5da99797e0f987ae4402c7617551efdfd393319a
Unknown <thangytp@gmail.com>
2018-08-21 18:09:24 +0700

    update route manga

--- server/controller/v1/manga.js
+++ server/controller/v1/manga.js
@@ -23,7 +23,44 @@ class MangaController {
 	}
 
 	async create(req, res) {
+		console.log(`Add new manga ${req.body.name}`);
+		let name = req.body.name;
+		let slug = req.body.slug;
+		let other_name = req.body.other_name;
+		let status = req.body.status;
+		let description = req.body.description;
+		let cover = req.body.cover;
+		// let {name, email, email} = {req.body.name, req.body.email, req.body.password};
 
+		//validate data
+		if (!name || !slug || !status || !cover) {
+			res.send(ResponseTemplate.error({
+				"message": "Cannot empty",
+				'code': EXISTED_CODE
+			}));
+		}
+
+		let mangaFind = await Manga.where({name: name}).fetch();
+		if (mangaFind) {
+			return res.status(200).json({"message": "Manga existed", 'code': EXISTED_CODE, "data": mangaFind});
+		}
+
+		let manga = new Manga({
+			name,
+			slug,
+			other_name,
+			status,
+			description,
+			cover
+		});
+		await manga.save()
+		.then(manga => {
+			res.status(200).json({"message": "Create manga successfully", "code": SUCCESS_CODE,"data": manga});
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(400).send({"message": 'Unable to create manga', "code": EXISTED_CODE, 'data': ''});
+		});
 	}
 
 	async retrieve(req, res) {
@@ -32,7 +69,11 @@ class MangaController {
 
 		}
 
-		return res.status(200).send(ResponseTemplate.success());
+		return res.status(200).send(ResponseTemplate.success({
+			'message': 'Success',
+			'code': SUCCESS_CODE,
+			'data': manga
+		}));
 	}
 
 	async delete(req, res) {
